1b34706cf0c5eff8dcc584a5903997df1ef0c175
Gabriel-Boiza <gabriel.boiza@inslapineda.cat>
2025-05-08 20:21:27 +0200

    middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,21 +1,14 @@
 import jwt from "jsonwebtoken"
 
 export const verifyToken = async (req, res, next) => {
-    const token = req.cookies.access_token;
-    console.log("Token recibido:", token);  // Verifica que el token esté presente
-
+    const token = req.cookies.access_token
     try {
-        if (!token) {
-            throw new Error("Any token");
-        }
         
-        const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY);
-        console.log("Token decodificado:", decoded);  // Imprime el contenido del token
-
-        req.user = decoded;
-        next();
+        if(!token){throw new Error("Any token")}
+        const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY)
+        req.user = decoded
+        next()
     } catch (error) {
-        console.error("Error al verificar el token:", error);  // Agrega un log detallado de los errores
-        res.status(401).json({ message: error.message });
+        res.status(401).json({message: error.message})
     }
-};
+}
